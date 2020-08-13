48cad1d6fc26f6197dbba7e846321e8081a02822
what1s1ove <zubko.2007@gmail.com>
2020-08-13 20:38:40 +0300

    module4-task1: + destination-info class

--- src/helpers/dom/create-element/create-element.js
+++ src/helpers/dom/create-element/create-element.js
@@ -2,7 +2,7 @@ const createElement = (template) => {
   const newElement = document.createElement(`div`);
   newElement.innerHTML = template;
 
-  return newElement.firstChild;
+  return newElement.firstElementChild;
 };
 
 export {createElement};
--- src/main.js
+++ src/main.js
@@ -1,14 +1,6 @@
-import {createDestinationInfoTemplate} from '~/view/destination-info/destination-info';
-import {createTripPriceTemplate} from '~/view/trip-price/trip-price';
-import {createSiteMenuTemplate} from '~/view/site-menu/site-menu';
-import {createFilterTemplate} from '~/view/filter/filter';
-import {createFormSortTemplate} from '~/view/form-sort/form-sort';
-import {createFormEventTemplate} from '~/view/form-event/form-event';
-import {createTripDaysTemplate} from '~/view/trip-days/trip-days';
-import {createTripDayTemplate} from '~/view/trip-day/trip-day';
-import {createEventTemplate} from '~/view/event/event';
 import {
   renderTemplate,
+  renderElement,
   generateEvents,
   getUniqueTripDays,
   getFixedDate,
@@ -17,22 +9,34 @@ import {
   getTotalPrice,
 } from '~/helpers';
 import {RenderPosition, SortOrder} from '~/common/enums';
+import DestinationInfoView from '~/view/destination-info/destination-info';
+import {createTripPriceTemplate} from '~/view/trip-price/trip-price';
+import {createSiteMenuTemplate} from '~/view/site-menu/site-menu';
+import {createFilterTemplate} from '~/view/filter/filter';
+import {createFormSortTemplate} from '~/view/form-sort/form-sort';
+import {createFormEventTemplate} from '~/view/form-event/form-event';
+import {createTripDaysTemplate} from '~/view/trip-days/trip-days';
+import {createTripDayTemplate} from '~/view/trip-day/trip-day';
+import {createEventTemplate} from '~/view/event/event';
 
 const EVENTS_COUNT = 20;
+
 const events = generateEvents(EVENTS_COUNT);
 const tripDays = getUniqueTripDays(events);
 const cities = getUniqueCities(events);
 const sortedStartDays = getSortedDates(SortOrder.DESK, tripDays.start);
 const totalPrice = getTotalPrice(events);
 
+const destinationInfoNode = new DestinationInfoView(cities, tripDays).node;
+
 const tripMaiNode = document.querySelector(`.trip-main`);
 const menuTitleNode = tripMaiNode.querySelector(`.trip-main__menu-title`);
 const filterTitleNode = tripMaiNode.querySelector(`.trip-main__filter-title`);
 const eventsContainerNode = document.querySelector(`.trip-events`);
 
-renderTemplate(
+renderElement(
     tripMaiNode,
-    createDestinationInfoTemplate(cities, tripDays),
+    destinationInfoNode,
     RenderPosition.AFTER_BEGIN
 );
 
